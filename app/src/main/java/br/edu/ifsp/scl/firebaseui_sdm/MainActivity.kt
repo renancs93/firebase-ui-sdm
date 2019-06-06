e30e86c52edbbd03315cb1310a5974da5c3f5529
Renan Silva <renancs93@gmail.com>
2019-06-06 01:16:31 -0300

    firebaseUI implementação

--- app/src/main/java/br/edu/ifsp/scl/firebaseui_sdm/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/firebaseui_sdm/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.edu.ifsp.scl.firebaseui_sdm
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
@@ -12,9 +13,14 @@ import android.support.v7.widget.Toolbar
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.ErrorCodes
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
+import com.kotlinpermissions.KotlinPermissions
+import kotlinx.android.synthetic.main.content_main.*
+
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -23,12 +29,15 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         AuthUI.IdpConfig.EmailBuilder().build(),
         AuthUI.IdpConfig.PhoneBuilder().build(),
         AuthUI.IdpConfig.GoogleBuilder().build(),
+        //AuthUI.IdpConfig.GitHubBuilder().build(),
         AuthUI.IdpConfig.FacebookBuilder().build(),
-        AuthUI.IdpConfig.TwitterBuilder().build())
+        AuthUI.IdpConfig.TwitterBuilder().build()
+    )
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
@@ -41,47 +50,81 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
+        permissions()
         navView.setNavigationItemSelectedListener(this)
 
-        paginaLogin()
-
+        verificaStatus()
 
     }
 
+
+    fun verificaStatus(){
+        if(FirebaseAuth.getInstance().currentUser == null){
+            paginaLogin()
+        }
+        else{
+            atualizaUI()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+    }
+
+    //Cria-se uma Intent do Firebase UI com as opçãos de Login
     fun paginaLogin(){
         // Create and launch sign-in intent
         startActivityForResult(AuthUI.getInstance()
             .createSignInIntentBuilder()
             .setAvailableProviders(providers)
-            .build(), RC_SIGN_IN
+            .setLogo(R.drawable.if_logo)//Inserido Logo Custom
+            .build()
+            , RC_SIGN_IN
         )
+
     }
 
+    //Recebe as informações de status do Login para redirecionamento
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == RC_SIGN_IN){
             val response = IdpResponse.fromResultIntent(data);
             if (resultCode == Activity.RESULT_OK) {
                 // Successfully signed in
                 val user = FirebaseAuth.getInstance().currentUser
-                Log.d("USER", user.toString());
+                atualizaUI()
                 // ...
             } else {
                 // Sign in failed. If response is null the user canceled the
                 // sign-in flow using the back button. Otherwise check
                 // response.getError().getErrorCode() and handle the error.
                 // ...
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    Toast.makeText(this, R.string.sign_in_cancelled, Toast.LENGTH_SHORT).show()
+                    return;
+                }
+
+                if (response.getError()?.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    Toast.makeText(this, response.error?.message , Toast.LENGTH_SHORT).show()
+                    return;
+                }
+                Toast.makeText(this, R.string.unknown_error, Toast.LENGTH_SHORT).show()
+                Log.e("ERROR-SIGNIN", "Sign-in error: ", response.getError());
             }
         }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
+    //Realiza o logoff do usuário
     private fun signOut() {
 
         AuthUI.getInstance()
             .signOut(this)
             .addOnCompleteListener{
-                paginaLogin()
+                verificaStatus()
             }
 
     }
@@ -89,6 +132,21 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     companion object {
 
         private const val RC_SIGN_IN = 123
+    }
+
+    private fun atualizaUI(){
+
+        if (FirebaseAuth.getInstance().currentUser != null){
+
+            val user = FirebaseAuth.getInstance().currentUser
+
+            txt_id.text = user?.uid
+            txt_email.text = user?.email
+            txt_nome.text = user?.displayName
+            txt_numeroCel.text = user?.phoneNumber
+
+        }
+
     }
 
     override fun onBackPressed() {
@@ -122,24 +180,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_home -> {
-                // Handle the camera action
+            R.id.nav_exit -> {
+                signOut()
             }
-            R.id.nav_gallery -> {
 
-            }
-            R.id.nav_slideshow -> {
-
-            }
-            R.id.nav_tools -> {
-
-            }
-            R.id.nav_share -> {
-
-            }
-            R.id.nav_send -> {
-
-            }
         }
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         drawerLayout.closeDrawer(GravityCompat.START)
@@ -147,7 +191,12 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
 
+    private fun permissions(){
 
+        KotlinPermissions.with(this)
+            .permissions(Manifest.permission.INTERNET)
+            .ask()
+    }
 
 
 }
